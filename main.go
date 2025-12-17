c7ba5213bc828ed63feaeacdde486f67630333bf
Alexey Yaroshenko <alexy.yaroshenko05@icloud.com>
2025-12-17 16:30:28 +0300

    sqr

--- main.go
+++ main.go
@@ -12,6 +12,18 @@ func generator(nums []int) <-chan int {
 	return ch
 }
 
+func sqr(ch <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for c := range ch {
+			out <- c * c
+		}
+	}()
+
+	return out
+}
+
 func main() {
 
 }
